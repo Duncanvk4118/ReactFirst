c6c5f6c62b2eba7575f89a9dce62b44222958d4e
Duncanvk4118 <97104118@st.deltion.nl>
2024-04-24 12:06:11 +0200

    last

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
+  const history = useHistory();
   const { id } = useParams();
   const {
     data: blog,
     isPending,
     error,
   } = useFetch("http://localhost:8000/blogs/" + id);
+
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => history.push("/"));
+  };
 
   return (
     <div className="blog-details">
@@ -19,6 +26,7 @@ const BlogDetails = () => {
           <h5>Written by: {blog.author}</h5>
           <br />
           <div>{blog.body}</div>
+          <button onClick={() => handleClick()}>Delete</button>
         </article>
       )}
     </div>
