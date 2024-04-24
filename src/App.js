477efb26f87c72e6f8f953f89ad95754f9c3cc49
Duncanvk4118 <97104118@st.deltion.nl>
2024-04-24 09:32:04 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Home from "./Home";
 import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   return (
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,8 +4,17 @@ const Navbar = () => {
       <h1>Posay Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create">New Blog</a>
-        <a href="/posay">Posay</a>
+        <a
+          href="/create"
+          style={{
+            color: "#f1356d",
+            border: "1px solid #f1356d",
+            borderRadius: "8px",
+          }}
+        >
+          New Blog
+        </a>
+        {/* <a href="/posay">Posay</a> */}
       </div>
     </nav>
   );
